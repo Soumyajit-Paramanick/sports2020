9ccb1e71b03781249009a513bd432b19d41c825a
Soumyajit-Paramanick <soumyajitparamanick96419@gmail.com>
2025-08-05 13:01:10 +0530

    serializers.py and views.py is modified just like in first stage

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -2,17 +2,9 @@ from rest_framework import serializers
 from .models import Product, ProductImage, Category, PromotionalBanner, GalleryImage, HomepageBanner
 
 class ProductImageSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-
     class Meta:
         model = ProductImage
         fields = ['image']
-
-    def get_image(self, obj):
-        request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url)
-        return obj.image.url
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,50 +14,146 @@ class CategorySerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     images = ProductImageSerializer(many=True, read_only=True)
     category = CategorySerializer(read_only=True)
-
+    
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'short_description', 'price', 'category', 'images']
 
 class PromotionalBannerSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
     is_currently_active = serializers.BooleanField(read_only=True)
-
+    image = serializers.ImageField(read_only=True)
+    
     class Meta:
         model = PromotionalBanner
         fields = ['id', 'message', 'is_active', 'start_time', 'end_time', 'is_currently_active', 'image', 'width', 'height']
 
-    def get_image(self, obj):
-        request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url)
-        return obj.image.url
-
 class GalleryImageSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-
     class Meta:
         model = GalleryImage
         fields = ['id', 'title', 'image', 'category']
 
-    def get_image(self, obj):
-        request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url)
-        return obj.image.url
-
 class HomepageBannerSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-
+    image = serializers.ImageField(read_only=True)
     class Meta:
         model = HomepageBanner
         fields = ['id', 'image', 'width', 'height', 'is_active']
 
-    def get_image(self, obj):
-        request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url)
-        return obj.image.url
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from rest_framework import serializers
+# from .models import Product, ProductImage, Category, PromotionalBanner, GalleryImage, HomepageBanner
+
+# class ProductImageSerializer(serializers.ModelSerializer):
+#     image = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = ProductImage
+#         fields = ['image']
+
+#     def get_image(self, obj):
+#         request = self.context.get('request')
+#         if request:
+#             return request.build_absolute_uri(obj.image.url)
+#         return obj.image.url
+
+# class CategorySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Category
+#         fields = ['id', 'name']
+
+# class ProductSerializer(serializers.ModelSerializer):
+#     images = ProductImageSerializer(many=True, read_only=True)
+#     category = CategorySerializer(read_only=True)
+
+#     class Meta:
+#         model = Product
+#         fields = ['id', 'name', 'description', 'short_description', 'price', 'category', 'images']
+
+# class PromotionalBannerSerializer(serializers.ModelSerializer):
+#     image = serializers.SerializerMethodField()
+#     is_currently_active = serializers.BooleanField(read_only=True)
+
+#     class Meta:
+#         model = PromotionalBanner
+#         fields = ['id', 'message', 'is_active', 'start_time', 'end_time', 'is_currently_active', 'image', 'width', 'height']
+
+#     def get_image(self, obj):
+#         request = self.context.get('request')
+#         if request:
+#             return request.build_absolute_uri(obj.image.url)
+#         return obj.image.url
+
+# class GalleryImageSerializer(serializers.ModelSerializer):
+#     image = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = GalleryImage
+#         fields = ['id', 'title', 'image', 'category']
+
+#     def get_image(self, obj):
+#         request = self.context.get('request')
+#         if request:
+#             return request.build_absolute_uri(obj.image.url)
+#         return obj.image.url
+
+# class HomepageBannerSerializer(serializers.ModelSerializer):
+#     image = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = HomepageBanner
+#         fields = ['id', 'image', 'width', 'height', 'is_active']
+
+#     def get_image(self, obj):
+#         request = self.context.get('request')
+#         if request:
+#             return request.build_absolute_uri(obj.image.url)
+#         return obj.image.url
 
 
 
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,69 +1,175 @@
-from rest_framework import generics
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
-from .models import Product, Category, PromotionalBanner, GalleryImage, HomepageBanner
-from .serializers import (
-    ProductSerializer,
-    CategorySerializer,
-    PromotionalBannerSerializer,
-    GalleryImageSerializer,
-    HomepageBannerSerializer,
-)
+# from rest_framework import generics
+# from django_filters.rest_framework import DjangoFilterBackend
+# from rest_framework import filters
+# from .models import Product, Category
+# from .serializers import ProductSerializer, CategorySerializer
 
-class ProductList(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['category']
-    search_fields = ['name', 'description']
+# class ProductList(generics.ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+#     filterset_fields = ['category']
+#     search_fields = ['name', 'description']
+    
+#     def get_queryset(self):
+#         queryset = super().get_queryset()
+#         min_price = self.request.query_params.get('min_price')
+#         max_price = self.request.query_params.get('max_price')
+        
+#         if min_price:
+#             queryset = queryset.filter(price__gte=min_price)
+#         if max_price:
+#             queryset = queryset.filter(price__lte=max_price)
+            
+#         return queryset
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        min_price = self.request.query_params.get('min_price')
-        max_price = self.request.query_params.get('max_price')
-        if min_price:
-            queryset = queryset.filter(price__gte=min_price)
-        if max_price:
-            queryset = queryset.filter(price__lte=max_price)
-        return queryset
+# class ProductDetail(generics.RetrieveAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
-    def get_serializer_context(self):
-        return {'request': self.request}
+# class CategoryList(generics.ListAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
 
-class ProductDetail(generics.RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
-    def get_serializer_context(self):
-        return {'request': self.request}
+# from .models import PromotionalBanner
+# from .serializers import PromotionalBannerSerializer
+# from django.utils import timezone
+# class PromotionalBannerList(generics.ListAPIView):
+#     queryset = PromotionalBanner.objects.filter(is_active=True)
+#     serializer_class = PromotionalBannerSerializer
 
-class CategoryList(generics.ListAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+#     def get_queryset(self):
+#         return super().get_queryset()
 
-    def get_serializer_context(self):
-        return {'request': self.request}
+# from .models import GalleryImage
+# from .serializers import GalleryImageSerializer
 
-class PromotionalBannerList(generics.ListAPIView):
-    queryset = PromotionalBanner.objects.filter(is_active=True)
-    serializer_class = PromotionalBannerSerializer
+# class GalleryImages(generics.ListAPIView):
+#     queryset = GalleryImage.objects.filter(is_active=True)
+#     serializer_class = GalleryImageSerializer
 
-    def get_serializer_context(self):
-        return {'request': self.request}
+# from .models import HomepageBanner
+# from .serializers import HomepageBannerSerializer
 
-class GalleryImages(generics.ListAPIView):
-    queryset = GalleryImage.objects.filter(is_active=True)
-    serializer_class = GalleryImageSerializer
+# class HomepageBannerList(generics.ListAPIView):
+#     queryset = HomepageBanner.objects.filter(is_active=True)
+#     serializer_class = HomepageBannerSerializer
 
-    def get_serializer_context(self):
-        return {'request': self.request}
 
-class HomepageBannerList(generics.ListAPIView):
-    queryset = HomepageBanner.objects.filter(is_active=True)
-    serializer_class = HomepageBannerSerializer
 
-    def get_serializer_context(self):
-        return {'request': self.request}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from rest_framework import generics
+# from django_filters.rest_framework import DjangoFilterBackend
+# from rest_framework import filters
+# from .models import Product, Category, PromotionalBanner, GalleryImage, HomepageBanner
+# from .serializers import (
+#     ProductSerializer,
+#     CategorySerializer,
+#     PromotionalBannerSerializer,
+#     GalleryImageSerializer,
+#     HomepageBannerSerializer,
+# )
+
+# class ProductList(generics.ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+#     filterset_fields = ['category']
+#     search_fields = ['name', 'description']
+
+#     def get_queryset(self):
+#         queryset = super().get_queryset()
+#         min_price = self.request.query_params.get('min_price')
+#         max_price = self.request.query_params.get('max_price')
+#         if min_price:
+#             queryset = queryset.filter(price__gte=min_price)
+#         if max_price:
+#             queryset = queryset.filter(price__lte=max_price)
+#         return queryset
+
+#     def get_serializer_context(self):
+#         return {'request': self.request}
+
+# class ProductDetail(generics.RetrieveAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+#     def get_serializer_context(self):
+#         return {'request': self.request}
+
+# class CategoryList(generics.ListAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+
+#     def get_serializer_context(self):
+#         return {'request': self.request}
+
+# class PromotionalBannerList(generics.ListAPIView):
+#     queryset = PromotionalBanner.objects.filter(is_active=True)
+#     serializer_class = PromotionalBannerSerializer
+
+#     def get_serializer_context(self):
+#         return {'request': self.request}
+
+# class GalleryImages(generics.ListAPIView):
+#     queryset = GalleryImage.objects.filter(is_active=True)
+#     serializer_class = GalleryImageSerializer
+
+#     def get_serializer_context(self):
+#         return {'request': self.request}
+
+# class HomepageBannerList(generics.ListAPIView):
+#     queryset = HomepageBanner.objects.filter(is_active=True)
+#     serializer_class = HomepageBannerSerializer
+
+#     def get_serializer_context(self):
+#         return {'request': self.request}
 
 
 
