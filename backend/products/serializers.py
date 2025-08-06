217f2aa23f2c2b6fd3cdb71fc36b0993959cdd5c
Soumyajit-Paramanick <soumyajitparamanick96419@gmail.com>
2025-08-06 10:12:24 +0530

    solving the problem of promotional banner by updating the serializers.py code

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -10,9 +10,12 @@ class ProductImageSerializer(serializers.ModelSerializer):
 
     def get_image(self, obj):
         request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url).replace("http://", "https://")
-        return obj.image.url
+        if obj.image:
+            image_url = obj.image.url
+            if request:
+                return request.build_absolute_uri(image_url).replace("http://", "https://")
+            return image_url
+        return None
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -40,9 +43,12 @@ class PromotionalBannerSerializer(serializers.ModelSerializer):
 
     def get_image(self, obj):
         request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url).replace("http://", "https://")
-        return obj.image.url
+        if obj.image:
+            image_url = obj.image.url
+            if request:
+                return request.build_absolute_uri(image_url).replace("http://", "https://")
+            return image_url
+        return None
 
 
 class GalleryImageSerializer(serializers.ModelSerializer):
@@ -54,9 +60,12 @@ class GalleryImageSerializer(serializers.ModelSerializer):
 
     def get_image(self, obj):
         request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url).replace("http://", "https://")
-        return obj.image.url
+        if obj.image:
+            image_url = obj.image.url
+            if request:
+                return request.build_absolute_uri(image_url).replace("http://", "https://")
+            return image_url
+        return None
 
 
 class HomepageBannerSerializer(serializers.ModelSerializer):
@@ -68,9 +77,108 @@ class HomepageBannerSerializer(serializers.ModelSerializer):
 
     def get_image(self, obj):
         request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url).replace("http://", "https://")
-        return obj.image.url
+        if obj.image:
+            image_url = obj.image.url
+            if request:
+                return request.build_absolute_uri(image_url).replace("http://", "https://")
+            return image_url
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from rest_framework import serializers
+# from .models import Product, ProductImage, Category, PromotionalBanner, GalleryImage, HomepageBanner
+
+# class ProductImageSerializer(serializers.ModelSerializer):
+#     image = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = ProductImage
+#         fields = ['image']
+
+#     def get_image(self, obj):
+#         request = self.context.get('request')
+#         if request:
+#             return request.build_absolute_uri(obj.image.url).replace("http://", "https://")
+#         return obj.image.url
+
+
+# class CategorySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Category
+#         fields = ['id', 'name']
+
+
+# class ProductSerializer(serializers.ModelSerializer):
+#     images = ProductImageSerializer(many=True, read_only=True)
+#     category = CategorySerializer(read_only=True)
+
+#     class Meta:
+#         model = Product
+#         fields = ['id', 'name', 'description', 'short_description', 'price', 'category', 'images']
+
+
+# class PromotionalBannerSerializer(serializers.ModelSerializer):
+#     is_currently_active = serializers.BooleanField(read_only=True)
+#     image = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = PromotionalBanner
+#         fields = ['id', 'message', 'is_active', 'start_time', 'end_time', 'is_currently_active', 'image', 'width', 'height']
+
+#     def get_image(self, obj):
+#         request = self.context.get('request')
+#         if request:
+#             return request.build_absolute_uri(obj.image.url).replace("http://", "https://")
+#         return obj.image.url
+
+
+# class GalleryImageSerializer(serializers.ModelSerializer):
+#     image = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = GalleryImage
+#         fields = ['id', 'title', 'image', 'category']
+
+#     def get_image(self, obj):
+#         request = self.context.get('request')
+#         if request:
+#             return request.build_absolute_uri(obj.image.url).replace("http://", "https://")
+#         return obj.image.url
+
+
+# class HomepageBannerSerializer(serializers.ModelSerializer):
+#     image = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = HomepageBanner
+#         fields = ['id', 'image', 'width', 'height', 'is_active']
+
+#     def get_image(self, obj):
+#         request = self.context.get('request')
+#         if request:
+#             return request.build_absolute_uri(obj.image.url).replace("http://", "https://")
+#         return obj.image.url
 
 
 
